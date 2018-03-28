c52e6b1bc5d2f2aff6cacdf89acf112b28465519
Leon Nikitin <nlv@lab321.ru>
2018-03-28 19:02:25 +0600

    init

---
+++ manifest.php
@@ -0,0 +1,47 @@
+<?php
+$README = <<<RDME
+Общие списки значений для ОРГ2
+RDME;
+
+
+$manifest = array (
+	0 => array (
+		'acceptable_sugar_versions' => 	array ( ),
+	),
+	1 => array (
+		'acceptable_sugar_flavors' => array (
+			0 => 'CE',
+			1 => 'PRO',
+			2 => 'ENT',
+		),
+	),
+    'readme' => $README,
+	'key' => '',
+	'author' => 'nlv',
+	'description' => 'General doms for ORG2 orgs',
+	'icon' => '',
+	'is_uninstallable' => true,
+	'name' => 'org2_doms',
+	'published_date' => '2018-03-28',
+	'type' => 'module',
+	'version' => '1.0.0',
+  'dependencies' => array(
+  ),
+	'remove_tables' => 'prompt',
+);
+
+$installdefs = array (
+	'id' => 'org2_doms',
+	'relationships' =>  array (  ),
+
+	'copy' => array (
+	),
+  'language' => 
+  array (
+    array (
+      'from' => '<basepath>/SugarModules/application/language/ru_RU.lang.php',
+      'to_module' => 'application',
+      'language' => 'ru_RU',
+    ),  
+),
+);
